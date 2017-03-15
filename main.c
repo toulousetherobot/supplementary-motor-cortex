2c5152928dea7408e34f8ad23402cf5922cc3157
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-03-15 01:14:25 -0400

    Fixed Unintentional Truncation of Lines
    As explained by the Software Engineering Institute FIO20-C there is a risk to assuming a new line character is read by fgets() which can result in data truncation. Therefore implemented compliant solution as indicated on https://www.securecoding.cert.org/confluence/pages/viewpage.action?pageId=3226

--- main.c
+++ main.c
@@ -80,56 +80,108 @@ int main(int argc, char** argv)
 
   char buffer[MaxTextExtent];
 
+  int found_tool = 0;
+  int tool_number = 0;
+  tsRational *points;
+  size_t count = -1;
+  char *ret = NULL;
+  size_t full_length = 0;
+
   while (fgets(buffer, sizeof(buffer), file))
   {
     /* note that fgets don't strip the terminating \n, checking its
       presence would allow to handle lines longer that sizeof(buffer) */
 
-    char *pt;
-    pt = strtok (buffer, ";");
-    // printf("Tool No: '%d'\n", atoi(pt));
-
-    tsRational *points = malloc(sizeof(tsRational) * MaxCtrlPointsExtent);
-    size_t count = -1;
-
-    pt = strtok (NULL, ",");
-    while (pt != NULL) {
-      count++;
-      points[count] = atof(pt);
-      pt = strtok (NULL, ",");
-    }
-
-    if (count % 2 == 0)
+    size_t len = strlen(buffer);
+    if (SIZE_MAX - len - 1 < full_length)
     {
-      fprintf(stderr,"Error %s: Improperly Defined Bezier Curve\n", argv[0]);
-      exit(EXIT_FAILURE);
+      break;
     }
-
-    tsBSpline spline;
-    ts_bspline_new(
-      1,      /* degree of spline */
-      2,      /* dimension of each point */
-      (count+1)/2,      /* number of control points */
-      TS_CLAMPED, /* used to hit first and last control point */
-      &spline /* the spline to setup */
-    );
-
-    /* Setup control points. */
-    size_t i;
-    for (i = 0; i < count+1; i++)
+    char *r_temp = realloc(ret, full_length + len + 1);
+    if (r_temp == NULL)
     {
-      spline.ctrlp[i] = points[i];
+      break;
     }
+    ret = r_temp;
+    strcpy(ret + full_length, buffer); /* concatenate */
+    full_length += len;
+    
+    if (feof(stdin) || buffer[len-1] == '\n')
+    {
+      // Complete Toolpath Found
+      char *new_line_char = strchr(ret, '\n');
+      if (new_line_char != NULL || feof(file))
+      {
+        // Truncate Buffer
+        if (new_line_char != NULL)
+          *new_line_char = '\0';
 
-    size_t size;
-    tsRational **cartesian, **transformation;
-    cartesian = spline_to_cartesian(&spline, 0.1f, &size);
-    transformation = cartesian_to_motor_angles(cartesian, size);
+        // Reset Toolpath Variables
+        found_tool = 0;
+        points = malloc(sizeof(tsRational) * MaxCtrlPointsExtent);
+        count = -1;
+      }
 
-    // Clean Up
-    cartesian = destroy_cartesian(cartesian, size);
+      // Parsing Tool Number
+      char *pt;
+      if (!found_tool)
+      {
+        pt = strtok (ret, ";");
+        if (pt != NULL)
+        {
+          found_tool = 1;
+          tool_number = atoi(pt);
+        }
+      }
 
-    free(points);
+      // Parsing Control Points
+      if (found_tool)
+        pt = strtok (NULL, ",");
+      else
+        pt = strtok (ret, ",");
+      while (pt != NULL) 
+      {
+        count++;
+        points[count] = atof(pt);
+        pt = strtok (NULL, ",");
+      }
+
+      // Checking Compliance for Dimensions of Control Points
+      if (count % 2 == 0)
+      {
+        fprintf(stderr,"Error %s: Improperly Defined Bezier Curve\n", argv[0]);
+        exit(EXIT_FAILURE);
+      }
+
+      // Building Spline
+      tsBSpline spline;
+      ts_bspline_new(
+        1,      /* degree of spline */
+        2,      /* dimension of each point */
+        (count+1)/2,      /* number of control points */
+        TS_CLAMPED, /* used to hit first and last control point */
+        &spline /* the spline to setup */
+      );
+
+      // Setup Control Points
+      size_t i;
+      for (i = 0; i < count+1; i++)
+      {
+        spline.ctrlp[i] = points[i];
+      }
+
+      // Transform Spline to Inverse Kinematics
+      size_t size;
+      tsRational **cartesian, **transformation;
+      cartesian = spline_to_cartesian(&spline, 0.1f, &size);
+      transformation = cartesian_to_motor_angles(cartesian, size);
+
+      // Clean Up
+      cartesian = destroy_cartesian(cartesian, size);
+      free(points);
+      free(ret); ret = (char *) NULL;
+      full_length = 0;
+    }
   }
 
   if (!feof(file))
@@ -139,5 +191,6 @@ int main(int argc, char** argv)
   }
 
   fclose(file);
+  free(ret);
   return EXIT_SUCCESS;
 }
